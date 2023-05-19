c862cff5462bb3d456a6a93d4fa7731fea4ccd98
polin103109 <sabrinpolinece18@gmail.com>
2023-05-20 00:00:18 +0600

    add router

--- 002-express-server/server.js
+++ 002-express-server/server.js
@@ -1,20 +1,26 @@
 const express = require("express");
-const port = 3000;
+const port = 8000;
 
 const server = express();
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
-server.get("/test", (req, res) => {
+const router = express.Router();
+router.get("/test", (req, res) => {
   res.send("test data");
 });
 
-server.get("/name", (req, res) => {
+router.get("/name", (req, res) => {
   res.send("simple express server");
 });
-server.get("/person", (req, res) => {
+router.get("/person", (req, res) => {
   res.status(200).json({
     name: "sabrina",
     age: "24",
+    address: {
+      city: "Chittagong",
+      country: "Bangladesh",
+    },
   });
 });
+server.use("/api", router);
