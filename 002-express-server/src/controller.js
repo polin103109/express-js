c979835ea4f9374f09a18fb35de30258a791b807
polin103109 <sabrinpolinece18@gmail.com>
2023-05-20 12:28:54 +0600

    add services layer

--- 002-express-server/src/controller.js
+++ 002-express-server/src/controller.js
@@ -1,11 +1,5 @@
-const getUser = (req, res) => {
-  res.status(200).json({
-    name: "sabrina",
-    age: "24",
-    address: {
-      city: "Chittagong",
-      country: "Bangladesh",
-    },
-  });
-};
+const Services = require("./service");
+
+const getUser = (req, res) => res.status(200).json(Services.getUser());
+
 module.exports = { getUser };
---
+++ 002-express-server/src/service.js
@@ -0,0 +1,9 @@
+const getUser = () => ({
+  name: "sabrina",
+  age: "24",
+  address: {
+    city: "Chittagong",
+    country: "Bangladesh",
+  },
+});
+module.exports = { getUser };
