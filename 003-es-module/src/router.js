c8c526563bcdccf442ac00dbb7a1238845642df9
polin103109 <sabrinpolinece18@gmail.com>
2023-05-20 18:51:12 +0600

    edited on nodemon

--- 003-es-module/src/router.js
+++ 003-es-module/src/router.js
@@ -4,4 +4,8 @@ const router = Router();
 router.get("/hello", (req, res) => {
   res.status(200).send("hello from es module");
 });
+
+router.get("/test", (req, res) => {
+  res.status(200).send("booommmmm");
+});
 export { router };
